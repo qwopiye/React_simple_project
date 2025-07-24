016cc51b1bc6db7ac80c9b25bea35ce7db4fd952
Sohanur Rahaman Sohan <152248019+qwopiye@users.noreply.github.com>
2025-07-24 10:14:45 +0600

    Counter

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
 import React, { useState } from 'react'
-// // import STATE from './componens/state.js'
-// import CONDITION_RANGERING from './CONDITION_RANDERING/index.js'
-// import CONDITION_RANGRING from './CONDITION_RANDERING index.js'
-// import HOOKS_USES1 from './HOOKS_USES/index2'
-// import HOOKS_USES2 from './HOOKS_USES/index1'
-// import HOOKS_USES from './HOOKS_USES/index1'
-// import HOOKS_USES from './HOOKS_USES/index2'
+
 export default function App() {
   const[Count,setCount]=useState(0);
   const EvenHandeler=()=>{
-        // setCount((Count)=>Count+1)//1+1=2
-        // setCount((Count)=>Count+1)
-        //  setCount((Count)=>Count+1);//2+1=3
-         setCount(Count+1)
+         setCount((Count)=>Count+1)//1+1=2
+         setCount((Count)=>Count+1)
+         setCount((Count)=>Count+1);//2+1=3
+     
   }
   const EvenHandeler1=()=>{
-        // setCount((Count)=>Count-1);
-        // setCount((Count)=>Count-1);
-        // setCount((Count)=>Count-1);
-        setCount(Count-1);
+         setCount((Count)=>Count-1);
+         setCount((Count)=>Count-1);
+         setCount((Count)=>Count-1);
+    
   }
 
   return (
